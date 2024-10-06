7c19183eba4c81367f69b6b594603500349b1f30
lg05byuidaho <guelsonlaguerre@gmail.com>
2024-10-05 23:49:29 -0400

    Modification Added

--- scripts/home.js
+++ scripts/home.js
@@ -148,5 +148,13 @@ document.getElementById('totalCredits').textContent = `Total Credits: ${totalCre
 
 
 
+const navLinks = document.querySelectorAll('nav a');
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        navLinks.forEach(nav => nav.classList.remove('active'));
+        link.classList.add('active');
+    });
+});
+
 
 
