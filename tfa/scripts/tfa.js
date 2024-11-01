9874f6b7562bd85cabccfb7dad33296acda93dbf
lg05byuidaho <guelsonlaguerre@gmail.com>
2024-11-01 00:06:28 -0400

    Page Added

--- tfa/scripts/tfa.js
+++ tfa/scripts/tfa.js
@@ -101,3 +101,27 @@ document.getElementById('search-form').addEventListener('submit', async function
 });
 
 // Call other necessary functions here (e.g., updateLastModified)
+
+
+// About lazy
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    let lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+
+    if ("IntersectionObserver" in window) {
+        let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    let lazyImage = entry.target;
+                    lazyImage.src = lazyImage.dataset.src;
+                    lazyImage.classList.remove("lazy");
+                    lazyImageObserver.unobserve(lazyImage);
+                }
+            });
+        });
+
+        lazyImages.forEach(function(lazyImage) {
+            lazyImageObserver.observe(lazyImage);
+        });
+    }
